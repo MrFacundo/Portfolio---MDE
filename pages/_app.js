777a58e816c5c822a386811704e98f6a484834d4
MrFacundo <ftroitero@gmail.com>
2022-10-02 15:45:56 +0100

    added locomotive, framer motion, logo

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,39 @@
+import { LocomotiveScrollProvider } from 'react-locomotive-scroll'
+import { useRef } from 'react'
 import Navbar from '../components/Navbar'
+
 import '../styles/globals.css'
+// import '../styles/locomotive.css'
+import React from 'react'
+import 'locomotive-scroll/dist/locomotive-scroll.css'
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Navbar />
-      <Component {...pageProps} />
-    </>
+  const containerRef = useRef(null)
 
+  return (
+
+    <LocomotiveScrollProvider
+      options={
+        {
+          smooth: true,
+          // ... all available Locomotive Scroll instance options 
+        }
+      }
+      watch={
+        [
+          //..all the dependencies you want to watch to update the scroll.
+          //  Basicaly, you would want to watch page/location changes
+          //  For exemple, on Next.js you would want to watch properties like `router.asPath`
+          // (you may want to add more criterias if the instance should be update on locations with query parameters)
+        ]
+      }
+      containerRef={containerRef}
+    >
+      <main data-scroll-container ref={containerRef}>
+        <Navbar />
+        <Component {...pageProps} />
+      </main>
+    </LocomotiveScrollProvider>
   )
 }
 
